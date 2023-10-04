06027ef1ae2b0a87969a99a5961012e6d3b5fb3f
miguelpgz <115643934+miguelpgz@users.noreply.github.com>
2023-10-04 16:19:48 +0200

    Delete microanuncios directory

--- microanuncios/src/main/java/com/example/microanuncios/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.microanuncios.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.microanuncios.model.Categoria;
-
-public interface CategoriaRepository  extends JpaRepository<Categoria, Integer>{
-    
-}
--- microanuncios/src/main/java/com/example/microanuncios/service/CategoriaService.java
+++
@@ -1,5 +0,0 @@
-package com.example.microanuncios.service;
-
-public interface CategoriaService {
-    
-}
